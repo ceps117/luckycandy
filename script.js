2f9ee4ddb977a83c9aa514cb643c8184ded7e9c4
ceps117 <cristopherpivaralceps@gmail.com>
2026-05-23 06:18:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
-const ruleta = document.getElementById("ruleta");
-const resultado = document.getElementById("resultado");
+function girarRuleta(){
 
-function comprarDulce(nombreDulce){
+  const ruleta = document.getElementById('ruleta');
+  const resultado = document.getElementById('resultado');
 
-  resultado.innerHTML = `Compraste: ${nombreDulce} 🍭<br>Girando ruleta...`;
-
-  // Giro visual aleatorio
   const giro = 3600 + Math.floor(Math.random() * 720);
+
   ruleta.style.transform = `rotate(${giro}deg)`;
 
-  // Espera mientras gira
+  resultado.innerHTML = 'Girando...';
+
   setTimeout(() => {
 
-    const premio = obtenerPremio();
+    const random = Math.random() * 100;
 
-    resultado.innerHTML = `🎉 ¡Ganaste ${premio} dulce(s)! 🍬`;
+    let premio;
+
+    if(random < 90){
+      premio = 1;
+    }
+    else if(random < 95){
+      premio = 2;
+    }
+    else if(random < 98){
+      premio = 3;
+    }
+    else if(random < 99.5){
+      premio = 4;
+    }
+    else{
+      premio = 5;
+    }
+
+    resultado.innerHTML = `🎉 Ganaste ${premio} dulce(s)`;
 
   }, 4000);
-}
-
-function obtenerPremio(){
-
-  const random = Math.random() * 100;
-
-  // 90%
-  if(random < 90){
-    return 1;
-  }
-
-  // 5%
-  else if(random < 95){
-    return 2;
-  }
-
-  // 3%
-  else if(random < 98){
-    return 3;
-  }
-
-  // 1.5%
-  else if(random < 99.5){
-    return 4;
-  }
-
-  // 0.5%
-  else{
-    return 5;
-  }
 }
